47504801fc7685d8774656f9df0ce5eea3b84679
batuhanyndny <batuhanyndny@gmail.com>
2021-10-09 20:59:21 +0300

    ContentWrapper Component Created

---
+++ src/components/Shared/ContentWrapper/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 2rem 0;
+  position: relative;
+`;
+
+export default ContentWrapper;
