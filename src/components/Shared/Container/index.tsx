cad77e72ce1fd6f3a59c27786b7d5e226e8ac2ef
batuhanyndny <batuhanyndny@gmail.com>
2021-10-09 20:59:02 +0300

    Container Component Created

---
+++ src/components/Shared/Container/index.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+interface ContainerProps {
+  flex?: boolean;
+  center?: boolean;
+  between?: boolean;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+  display: ${({ flex }) => (flex ? 'flex' : 'block')};
+
+  ${({ center }) =>
+    center &&
+    `
+    justify-content: center;
+    align-items: center;
+  `}
+
+  ${({ between }) =>
+    between &&
+    `
+    justify-content: space-between;
+    align-items: center;
+  `}
+
+  @media (min-width: 640px) {
+    max-width: 640px;
+  }
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
+  @media (min-width: 1280px) {
+    max-width: 1280px;
+  }
+  @media (min-width: 1536px) {
+    max-width: 1536px;
+  }
+`;
+
+export default Container;
