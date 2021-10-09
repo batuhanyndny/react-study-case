eb5dfe9ed7ca1823dc35fc815bed31240c7e42d8
batuhanyndny <batuhanyndny@gmail.com>
2021-10-09 21:05:56 +0300

    [Redux] Update rootSaga wrt new sagas

--- src/redux/sagas/rootSaga.ts
+++ src/redux/sagas/rootSaga.ts
@@ -1,7 +1,9 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, all } from 'redux-saga/effects';
 import { GET_USER } from '../ducks/user';
 import { handleGetUser } from './user';
+import { handleGetItems } from './item';
+import { GET_ITEMS } from '../ducks/item';
 
 export function* rootSaga() {
-  yield takeLatest(GET_USER, handleGetUser);
+  yield all([takeLatest(GET_USER, handleGetUser), takeLatest(GET_ITEMS, handleGetItems)]);
 }
