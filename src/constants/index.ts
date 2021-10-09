127808bbf87ba9ce0725de4229982d19b4f9670a
batuhanyndny <batuhanyndny@gmail.com>
2021-10-09 21:00:32 +0300

    Constants updated

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,17 @@
-const constants = {
-  baseUrl: 'https://my-json-server.typicode.com/batuhanyndny/foo/',
+interface ITheme {
+  primary: string;
+  secondary: string;
+  title: string;
+  pageBackground: string;
+}
+
+export const constants = {
+  baseUrl: 'http://localhost:3001/',
 };
 
-export default constants;
+export const theme: ITheme = {
+  primary: '#1EA4CE',
+  secondary: '#147594',
+  title: '#6F6F6F',
+  pageBackground: '#fafafa',
+};
