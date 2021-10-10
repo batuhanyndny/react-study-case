e59c34c293bf6d4a90e3201cafe6400097d3aabc
batuhanyndny <batuhanyndny@gmail.com>
2021-10-10 22:56:32 +0300

    Pagination component created

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,105 @@
+import React, { useMemo } from 'react';
+import ArrowIcon from '../Shared/Arrow';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../redux';
+import {
+  PageNumber,
+  PageNumberContainer,
+  PaginationButton,
+  PaginationContainer,
+  PaginationDots,
+  StyledPagination,
+} from './style';
+import { PER_PAGE } from '../../constants';
+import { setPage } from '../../redux/ducks/pagination';
+
+const Pagination = () => {
+  const pagination = useSelector((state: RootState) => state.pagination);
+  const items = useSelector((state: RootState) => state.items);
+  const dispatch = useDispatch();
+
+  const maxPageCount = useMemo(() => {
+    return Math.round(items.length / PER_PAGE);
+  }, [items]);
+
+  const pages = useMemo(() => {
+    return Array.from({ length: maxPageCount }, (_, i) => i + 1);
+  }, [maxPageCount]);
+
+  return (
+    <PaginationContainer>
+      <StyledPagination>
+        <PaginationButton
+          active={pagination.page !== 1}
+          disabled={pagination.page === 1}
+          onClick={() => {
+            dispatch(setPage(pagination.page - 1));
+          }}
+        >
+          <ArrowIcon active={pagination.page !== 1} />
+          Prev
+        </PaginationButton>
+        <PageNumberContainer>
+          {pages.map((pageNumber, idx) => {
+            if ([pages[0], pages[1]].includes(pageNumber))
+              return (
+                <PageNumber
+                  key={idx}
+                  onClick={() => {
+                    dispatch(setPage(pageNumber));
+                  }}
+                  active={pagination.page === pageNumber}
+                >
+                  {pageNumber}
+                </PageNumber>
+              );
+            if (pagination.page > 4 && idx === 2) {
+              return <PaginationDots key={idx}>...</PaginationDots>;
+            }
+            if (pagination.page - 1 <= pageNumber && pageNumber <= pagination.page + 2) {
+              return (
+                <PageNumber
+                  key={idx}
+                  onClick={() => {
+                    dispatch(setPage(pageNumber));
+                  }}
+                  active={pagination.page === pageNumber}
+                >
+                  {pageNumber}
+                </PageNumber>
+              );
+            }
+            if (pageNumber > pagination.page + 2 && idx === pagination.page + 2) {
+              return <PaginationDots key={idx}>...</PaginationDots>;
+            }
+            if ([pages[pages.length - 1], pages[pages.length - 2]].includes(pageNumber))
+              return (
+                <PageNumber
+                  key={idx}
+                  onClick={() => {
+                    dispatch(setPage(pageNumber));
+                  }}
+                  active={pagination.page === pageNumber}
+                >
+                  {pageNumber}
+                </PageNumber>
+              );
+            return null;
+          })}
+        </PageNumberContainer>
+        <PaginationButton
+          active={pagination.page !== maxPageCount}
+          disabled={pagination.page === maxPageCount}
+          onClick={() => {
+            dispatch(setPage(pagination.page + 1));
+          }}
+        >
+          <ArrowIcon right active={pagination.page !== maxPageCount} />
+          Next
+        </PaginationButton>
+      </StyledPagination>
+    </PaginationContainer>
+  );
+};
+
+export default Pagination;
---
+++ src/components/Pagination/style.tsx
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+interface ActiveInterface {
+  active?: boolean;
+}
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem auto;
+`;
+
+export const StyledPagination = styled.div`
+  display: flex;
+  min-width: 535px;
+  justify-content: space-between;
+`;
+
+export const PaginationButton = styled.button<ActiveInterface>`
+  cursor: pointer;
+  font-size: 14px;
+  background-color: transparent;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+  font-weight: 600;
+  color: ${({ active, theme }) => (active ? theme.primary : theme.gray)};
+`;
+
+export const PaginationDots = styled.div`
+  display: flex;
+  width: 32px;
+  min-width: 32px;
+  height: 40px;
+  min-height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PageNumberContainer = styled.div`
+  overflow: hidden;
+  display: flex;
+  min-width: 333px;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const PageNumber = styled.div<ActiveInterface>`
+  width: 32px;
+  min-width: 32px;
+  height: 40px;
+  min-height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  color: ${({ theme, active }) => (active ? '#fff' : theme.gray)};
+  background-color: ${({ theme, active }) => (active ? theme.primary : 'transparent')};
+`;
